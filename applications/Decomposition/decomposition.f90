6f43b332d5e2cfe42acc889a13d4d52616fa5469
nikola-m <largeddysimulation@gmail.com>
2023-02-10 17:07:32 +0100

    Corrections to Decomposition code.

--- applications/Decomposition/decomposition.f90
+++ applications/Decomposition/decomposition.f90
@@ -5,7 +5,7 @@
   !
   ! References:
   ! Details have been taken from PhD thesis of Bojan Ničeno at TU Delft (2001) 
-  ! (search online repository TU Delft library)
+  ! (search online repository TU Delft library).
   !
   !+-----------------------------------------------------------------------------+
 
@@ -18,12 +18,10 @@
   implicit none
 
   integer :: i
-  integer :: LWORK, INFO
-  real(dp) :: A( 3,3 ), VL( 3,3 ), VR( 3,3 ), WR( 3 ), WI( 3 ), WORK( 1000 )
 
   real(dp):: xm,ym,zm
   real(dp):: Ixx,Ixy,Ixz,Iyx,Iyy,Iyz,Izx,Izy,Izz
-  ! real(dp), dimension(3,3) :: A,lam,v
+  real(dp), dimension(3,3) :: A,lam,v
 
 !+-----------------------------------------------------------------------------+
 
@@ -53,11 +51,11 @@
   Ixx = sum( (yc(1:numCells)-ym)**2 + (zc(1:numCells)-zm)**2 )
   Ixy = sum( (xc(1:numCells)-xm)*(yc(1:numCells)-ym)**2 )
   Ixz = sum( (xc(1:numCells)-xm)*(zc(1:numCells)-zm)**2 )
-  Iyx = sum( (yc(1:numCells)-ym)*(xc(1:numCells)-xm)**2 )
+  Iyx = Ixy
   Iyy = sum( (xc(1:numCells)-xm)**2 + (zc(1:numCells)-zm)**2  )
   Iyz = sum( (yc(1:numCells)-ym)*(zc(1:numCells)-zm)**2 )
-  Izx = sum( (zc(1:numCells)-zm)*(xc(1:numCells)-xm)**2 )
-  Izy = sum( (zc(1:numCells)-zm)*(yc(1:numCells)-ym)**2 )
+  Izx = Ixz
+  Izy = Iyz
   Izz = sum( (yc(1:numCells)-ym)**2 + (xc(1:numCells)-xm)**2  )
 
 
@@ -69,36 +67,18 @@
 
   A = reshape([Ixx,Ixy,Ixz,Iyx,Iyy,Iyz,Izx,Izy,Izz],[3,3])
 
-  ! call eig(A,3,lam,v)
+  call eig(A,3,lam,v)
 
-  ! write(*,'(a)') ' Eigenvalues of the inertia matrix:'
-  ! write(*,'(3(e13.6,1x))') lam(1,1),lam(2,2),lam(3,3)
-  ! write(*,'(a)') ' '
-
-
-  ! All this but with Lapack
-  LWORK = -1
-  CALL DGEEV( 'Vectors', 'Vectors', 3, A, 3, WR, WI, VL, 3, &
-              VR, 3, WORK, LWORK, INFO )
-  LWORK = MIN( 1000, INT( WORK( 1 ) ) )
-  ! Solve eigenproblem:
-  CALL DGEEV( 'Vectors', 'Vectors', 3, A, 3, WR, WI, VL, 3, &
-              VR, 3, WORK, LWORK, INFO )
-
-  write(*,'(a)') ' Eigenvalues of the inertia matrix from LAPACK:'
-  write(*,'(3(e13.6,1x))') (WR(i), i=1,3)
-  write(*,'(3(e13.6,1x))') (WI(i), i=1,3)
+  write(*,'(a)') ' Eigenvalues of the inertia matrix:'
+  write(*,'(3(e13.6,1x))') lam(1,1),lam(2,2),lam(3,3)
   write(*,'(a)') ' '
 
-  write(*,'(3(e13.6,1x))') (VL(1,i), i=1,3)
-  write(*,'(3(e13.6,1x))') (VL(2,i), i=1,3)
-  write(*,'(3(e13.6,1x))') (VL(3,i), i=1,3)
+  write(*,'(a)') ' Eigenvectors of the inertia matrix:'
+  write(*,'(a,3(e13.6,1x),a)') '(',(V(i,1), i=1,3),')'
+  write(*,'(a,3(e13.6,1x),a)') '(',(V(i,2), i=1,3),')'
+  write(*,'(a,3(e13.6,1x),a)') '(',(V(i,3), i=1,3),')'
   write(*,'(a)') ' '
 
-  write(*,'(3(e13.6,1x))') (VR(1,i), i=1,3)
-  write(*,'(3(e13.6,1x))') (VR(2,i), i=1,3)
-  write(*,'(3(e13.6,1x))') (VR(3,i), i=1,3)
-  write(*,'(a)') ' '
 
   call say_goodbye( )
 
